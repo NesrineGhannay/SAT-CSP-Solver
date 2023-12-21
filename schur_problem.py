75b018179b2da874b4f7469b39afa85749aa10ee
g20006545 <nesrine.ghannay@etu.univ-amu.fr>
2023-12-21 09:57:46 +0100

    test Q4

--- schur_problem.py
+++ schur_problem.py
@@ -1,5 +1,7 @@
 from pycsp3 import *
 
+data = (10, 3)
+
 n = data[0]
 k = data[1]
 
@@ -7,6 +9,10 @@ b = VarArray(size=n, dom=range(1, k + 1))
 
 satisfy(
     (imply(b[i] == b[j], b[l] != b[i]))
-    for i in range(n + 1) for j in range(n + 1) for l in range(n + 1)
+    for i in range(n) for j in range(n) for l in range(n)
     if (i + j == l)
 )
+
+result = solve()
+
+print(result)
