c69d6703371de79bf31e5b0d26ba7e5f8719faff
ghann <nesrine.ghannay@etu.univ-amu.fr>
2024-01-24 14:19:30 +0100

    ça marche ??? Pauline la queen

--- schur_problem_sat_new.py
+++ schur_problem_sat_new.py
@@ -2,7 +2,6 @@ from collections import defaultdict
 import os
 
 
-
 def generate_schur_csp_instance(k, n):
     clauses = ""
 
@@ -27,6 +26,7 @@ def generate_schur_csp_instance(k, n):
     for i in range(1, n + 1):
         for j in range(1, k + 1):
             for l in range(j + 1, k + 1):
+                print("clause : - (i: ", i, " j: ", j, ") ou -(i: ", i, " l:", l, ")")
                 clauses += str(-formeInjective(i, j, k)) + " " + str(-formeInjective(i, l, k)) + " 0\n"
     return clauses
 
@@ -39,25 +39,64 @@ def formeInjective(i, j, nbBoxes):
     else:
         return (nbBoxes * (i - 1)) + j  # (i, j) --> (k*i-1) +j
 
+print("Forme Injective de 7, 1 : ", formeInjective(7, 1, 3))
+print("Forme Injective de 7, 2 : ",formeInjective(7, 2, 3))
+print("Forme Injective de 7, 3 : ",formeInjective(7, 3, 3))
+
+"""
+def formeInjective_inverse(x, nbBoxes):
+    if x < 0:
+        x = abs(x)
+        j = (x % nbBoxes) + 1
+        i = ((x - j - 1) // nbBoxes) + 1
+        return -i, -j
+    else:
+        j = (x % nbBoxes) + 1
+        i = ((x - j - 1) // nbBoxes) + 1
+        print("x : ", x, " et i, j : ", i, " , ", j)
+        return i, j
+"""
+
 
 def formeInjective_inverse(x, nbBoxes):
     if x < 0:
         x = abs(x)
-        j = (x % nbBoxes)
-        i = ((x - j) // nbBoxes) + 1
+        j = (x % nbBoxes) + 1
+        i = ((x - j - 1) // nbBoxes) + 1
         return -i, -j
     else:
-        j = (x % nbBoxes)
-        i = ((x - j) // nbBoxes) + 1
+        i = ((x - 1) // nbBoxes) + 1
+        j = ((x - 1) % nbBoxes) + 1
+        print("x : ", x, " et i, j : ", i, " , ", j)
         return i, j
 
-#print(formeInjective(-456, -10, 15))
-#print(formeInjective_inverse(-6835, 15))
+
+
+"""
+i = (x // nbBoxes) + 1
+j = x - ((i - 1) * nbBoxes)"""
+"""
+print("Reciproque 21 : ", formeInjective_inverse(21, 3))
+print("Reciproque 22 : ", formeInjective_inverse(22, 3))
+print("Reciproque 27 : ", formeInjective_inverse(27, 3))
+print("Reciproque 29 : ", formeInjective_inverse(29, 3))
+
+print("Reciproque -19 : ", formeInjective_inverse(-19, 3))
+print("Reciproque -20 : ", formeInjective_inverse(-20, 3))
+print("Reciproque 21 : ", formeInjective_inverse(21, 3))"""
+
+"""
+print(formeInjective(-456, -10, 15))
+print(formeInjective_inverse(-6835, 15))
+print(formeInjective(8, 0, 3))
+print(formeInjective(8, 1, 3))
+"""
 """
 print(formeInjective_inverse(2, 3))
 print(formeInjective_inverse(4, 3))
 print(formeInjective_inverse(7, 3))
 print(formeInjective_inverse(10, 3))"""
+
 
 def genererFichierClauses(k, n, fileName):
     clauses = generate_schur_csp_instance(k, n)
@@ -67,6 +106,7 @@ def genererFichierClauses(k, n, fileName):
     fichier.write(clauses)
     fichier.close()
 
+
 #genererFichierClauses(3, 20, "instances/3_20.cnf")
 
 
@@ -74,29 +114,33 @@ def show_solution(solutionFileName, k):
     with open(solutionFileName, 'r') as f:
         lines = f.readlines()
     if lines[0].strip() == "SAT":
-        literals = [int(val) for val in lines[1].split()[:-1]]  # Ignorer le dernier zéro
-        litteraux = [formeInjective_inverse(valuation, k) for valuation in literals]
+        literals = [int(val) for val in lines[1].split()[:-1]]
+        #print(" x : ", literals)
+        litteraux = [formeInjective_inverse(valuation, k) for valuation in literals if valuation > 0]
+        #print("i , j : ", litteraux)
 
-        boites = defaultdict(list)
-
-        """for balle, boite in litteraux:
-            if boite > 0 :
-                boites[boite].append(balle)
-
-        for i in range(1, k + 1):
-            print("Boîte {}: {}".format(i, boites[i]))
-            """
         boites = {i: [] for i in range(1, k + 1)}
-
+        c = 0
         for balle, boite in litteraux:
+            # print(litteraux)
             if boite > 0:
                 boites[boite].append(balle)
+                c += 1
 
         for i in range(1, k + 1):
             print("Boîte {}: {}".format(i, boites[i]))
+        print("Nombre d'affectations : " + str(c))
 
     else:
         print("La formule n'est pas satisfaisable.")
 
 
-#show_solution("solutions/3_20.out", 3)
+show_solution("solutions/3_4.out", 3)
+# problème :
+"""
+x :  21  et i, j :  8  ,  0
+x :  22  et i, j :  8  ,  1
+x :  27  et i, j :  10  ,  0
+x :  29  et i, j :  10  ,  2
+"""
+#(8, 0), (8, 1), (10, 0), (10, 2)
